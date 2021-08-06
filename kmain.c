e7b98eef4aefb065745b2217bfcee4c69eea846f
Kajanthan.s <73311101+sureshkajan@users.noreply.github.com>
2021-08-06 20:12:03 +0530

    Add files via upload

--- kmain.c
+++ kmain.c
@@ -1,5 +1,13 @@
-    /* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
-    {
-        return arg1 + arg2 + arg3;
-    }
+#include "drivers/frame_buffer.h"
+#include "drivers/serial_port.h"
+
+/* The C function */
+int sum_of_three(int arg1, int arg2, int arg3)
+{
+char arr[] = "welcome to kajanos";
+
+fb_write(arr, 19);
+serial_write(0x3F8, arr, 19);
+
+return arg1 + arg2 + arg3;
+}
