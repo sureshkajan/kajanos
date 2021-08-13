9bdee73d37c8ab169cd547c31759d24310acaf00
Kajanthan.s <73311101+sureshkajan@users.noreply.github.com>
2021-08-13 20:03:58 +0530

    integrate_segmentation

--- kmain.c
+++ kmain.c
@@ -1,9 +1,11 @@
 #include "drivers/frame_buffer.h"
 #include "drivers/serial_port.h"
+#include "segmentation/memory_segments.h"
 
 /* The C function */
 int sum_of_three(int arg1, int arg2, int arg3)
 {
+segments_install_gdt();
 char arr[] = "welcome to kajanos";
 
 fb_write(arr, 19);
